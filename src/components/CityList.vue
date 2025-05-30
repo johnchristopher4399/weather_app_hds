f93c0bed18c8a75ebbd0bf1da839102bf66e7495
东升 霍 <2689756703@qq.com>
2025-05-30 18:32:59 +0800

    completed app

---
+++ src/components/CityList.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+      <div v-for="city in savedCities" :key="city.id">
+        <CityCard :city="city" @click="goToCityView(city)" />
+      </div>
+
+      <p v-if="savedCities.length === 0">
+        没有新增地点。要开始跟踪位置，请在上面的字段中搜索。
+      </p>
+  </div>
+</template>
+
+<script setup>
+import axios from "axios";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import CityCard from "./CityCard.vue";
+
+const savedCities = ref([]);
+const getCities = async () => {
+  if (localStorage.getItem("savedCities")) {
+    savedCities.value = JSON.parse(
+      localStorage.getItem("savedCities")
+    );
+
+    const requests = [];
+    savedCities.value.forEach((city) => {
+      requests.push(
+        axios.get(
+          `https://api.openweathermap.org/data/3.0/weather?lat=${city.coords.lat}&lon=${city.coords.lng}&appid=7efa332cf48aeb9d2d391a51027f1a71&units=imperial`
+        )
+      );
+    });
+
+    const weatherData = await Promise.all(requests);
+
+    weatherData.forEach((value, index) => {
+      savedCities.value[index].weather = value.data;
+    });
+  }
+};
+await getCities();
+
+const router = useRouter();
+const goToCityView = (city) => {
+  router.push({
+    name: "cityView",
+    params: { state: city.state, city: city.city },
+    query: {
+      id: city.id,
+      lat: city.coords.lat,
+      lng: city.coords.lng,
+    },
+  });
+};
+</script>
